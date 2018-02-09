1fb08feff378d736d0f32b05dbbd8abcbe379825
Sam K <samuel.kortas@kaust.edu.sa>
2018-02-09 06:28:13 +0300

    deployement 0.9.2 on pipy

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.1',  # Required
+    version='0.9.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -105,8 +105,8 @@ setup(
         'Operating System :: POSIX',
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Clustering",
-        "Topic :: System :: Distributed Computing"
-        'Topic :: Utilities'
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Utilities"
 
     ],
 
